e91f88d1e6c2ac08f24575f6afb1648760588bc3
Reinaldo Antonio Camargo Rauch <reinaldorauch@gmail.com>
2014-02-20 00:28:30 -0300

    Arrumado bug do motor e da string, adicionado comentários nos métodos

--- c++/automobile.cpp
+++ c++/automobile.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 
 #include "engine.cpp"
 
@@ -34,46 +35,79 @@ class Automobile {
 	 * Métodos públicos
 	 */
 	public:
+
+		/**
+		 * Construtor simples
+		 */
 		Automobile() {
-			Engine e();
-			this->setEngine(e);
-			this->setYear(DEFAULT_YEAR);
-			this->setBrand(DEFAULT_BRAND);
+			Engine e;
+			std::string marca;
+			marca = DEFAULT_BRAND;
+			this->setEngine(&e);
+			this->setYear((unsigned)DEFAULT_YEAR);
+			this->setBrand(marca);
 		}
 
+		/**
+		 * Construtor setando os atributos
+		 */
 		Automobile(Engine *eng, unsigned y, std::string brn) {
 			this->setEngine(eng);
 			this->setYear(y);
 			this->setBrand(brn);
 		}
 
+		/**
+		 * Getters & Setters
+		 */
+
+		/**
+		 * Setter de marca
+		 * @param b String para a marca do automóvel
+		 */
 		void setBrand(std::string b) {
-			if(strlen(b) < 2)
+			if(b.length() < 2)
 				throw 1;
 
 			this->brand = b;
 		}
 
+		/**
+		 * getter da marca
+		 * @return retorna a marca do automóvel
+		 */
 		std::string getBrand() {
 
 			return this->brand;
 
 		}
 
+		/**
+		 * Setter do ano do automóvel
+		 * @param y ano do automóvel
+		 */
 		void setYear(unsigned y) {
-			if(y < 1800)
+			if(y < (unsigned) DEFAULT_YEAR)
 				throw 1;
 
 			this->year = y;
 		}
 
+		/**
+		 * Getter do ano do automóvel
+		 * @return ano do automóvel
+		 */
 		unsigned getYear() {
 
 			this->year;
 
 		}
 
-		void setEngine(Engine eng) {
+		/**
+		 * Setter do motor do automóvel
+		 * @param eng Motor do automóvel
+		 */
+		void setEngine(Engine *eng) {
 
 			if(!eng->isValid())
 				throw 2;
@@ -82,6 +116,10 @@ class Automobile {
 
 		}
 
+		/**
+		 * Getter do moto do automóvel
+		 * @return Motor do automóvel
+		 */
 		Engine *getEngine() {
 
 			return this->engine;
